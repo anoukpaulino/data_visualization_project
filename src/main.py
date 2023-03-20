c7b38f830e03af77e6e8aa7a5a8a510e2d458e86
Anouk <anouk.paulino@epfl.ch>
2023-03-20 17:21:35 +0100

    trying out some stuff with Swiss food database

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import plotly.express as px
 import pandas as pd
 import plotly.graph_objects as go
+import openpyxl
 
 
 def merge_and_create_plots(left_df, right_df, lcolumn_name, rcolumn_name):
@@ -13,20 +14,25 @@ def merge_and_create_plots(left_df, right_df, lcolumn_name, rcolumn_name):
     plot2.show()
 
 
-def descending_bar_plot(df, column_name):
-    fig = px.bar(df, x='Entity', y=column_name)
+def descending_bar_plot(df, rcolumn_name, lcolumn_name='Entity'):
+    fig = px.bar(df, x=lcolumn_name, y=rcolumn_name)
     fig.update_layout(xaxis={'categoryorder': 'total descending'})
     fig.show()
 
 
+def extract_type_from_column(df, column_name, type_name):
+    return df[df[column_name].isin([type_name])]
+
+
 if __name__ == "__main__":
+    """
     water = pd.read_csv('../data/WaterUse.csv')
     land = pd.read_csv('../data/LandUse.csv')
     scarcity = pd.read_csv('../data/Scarcity-WeightedWater.csv')
     carbon = pd.read_csv('../data/CarbonEmissions.csv')
     eutrophication = pd.read_csv('../data/Eutrophication.csv')
 
-    """
+    
     print(water.columns)
     print(land.columns)
     print(scarcity.columns)
@@ -45,6 +51,8 @@ if __name__ == "__main__":
     merge_and_create_plots(scarcity, eutrophication, 'Scarcity-weighted water use per kilogram (Poore & Nemecek, 2018)', 'Eutrophying emissions per kilogram (Poore & Nemecek, 2018)')
     """
 
+    # emissions by stage, grouped
+    """
     carbon_by_stage = pd.read_csv('../data/CarbonEmissionsByStage.csv')
     # print(carbon_by_stage.columns)
 
@@ -63,6 +71,7 @@ if __name__ == "__main__":
     )
 
     # stage.show()
+    """
 
     # bar charts with each stage independent and in descending order of emitting entity
     """
@@ -76,3 +85,89 @@ if __name__ == "__main__":
     descending_bar_plot(carbon_by_stage, 'food_emissions_losses')
     """
     # --> the most important emission stage is what they call Farm emissions, 2nd is food losses, 3rd is animal feed
+
+    nutrients = pd.read_excel('../data/Swiss_food_composition_database(4).xlsx')
+    print(nutrients.columns)
+
+    """
+    cereal = extract_type_from_column(nutrients, 'Category', 'Cereal products, pulses and potatoes/Other cereal products')
+    descending_bar_plot(cereal, 'Protein (g)', 'Name')
+    descending_bar_plot(cereal, 'Selenium (Se) (µg)', 'Name')
+    descending_bar_plot(cereal, 'Energy, kilojoules (kJ)', 'Name')
+    """
+
+    """
+    # Folate
+    fig = px.scatter(nutrients, y='Folate (µg)', x='Protein (g)', color='Category')
+    fig.show()
+
+    fig = px.scatter(nutrients, y='Folate (µg)', x='Starch (g)', color='Category')
+    fig.show()
+
+    fig = px.scatter(nutrients, y='Folate (µg)', x='Dietary fibres (g)', color='Category')
+    fig.show()
+
+    fig = px.scatter(nutrients, y='Folate (µg)', x='Fat, total (g)', color='Category')
+    fig.show()
+
+    # Vitamine A
+    fig = px.scatter(nutrients, y='Vitamin A activity, RE (µg-RE)', x='Starch (g)', color='Category')
+    fig.show()
+
+    fig = px.scatter(nutrients, y='Vitamin A activity, RE (µg-RE)', x='Protein (g)', color='Category')
+    fig.show()
+
+    fig = px.scatter(nutrients, y='Vitamin A activity, RE (µg-RE)',  x='Sugars (g)', color='Category')
+    fig.show()
+
+    fig = px.scatter(nutrients, y='Vitamin A activity, RE (µg-RE)', x='Dietary fibres (g)', color='Category')
+    fig.show()
+
+    fig = px.scatter(nutrients, y='Vitamin A activity, RE (µg-RE)', x='Fat, total (g)', color='Category')
+    fig.show()
+
+    # Zinc
+    # interesting one
+    fig = px.scatter(nutrients, y='Zinc (Zn) (mg)', x='Protein (g)', color='Category')
+    fig.show()
+
+    fig = px.scatter(nutrients, y='Zinc (Zn) (mg)', x='Starch (g)', color='Category')
+    fig.show()
+
+    fig = px.scatter(nutrients, y='Zinc (Zn) (mg)', x='Dietary fibres (g)', color='Category')
+    fig.show()
+
+    fig = px.scatter(nutrients, y='Zinc (Zn) (mg)', x='Fat, total (g)', color='Category')
+    fig.show()
+
+    # Iodide
+    fig = px.scatter(nutrients, y='Iodide (I) (µg)', x='Protein (g)', color='Category')
+    fig.show()
+
+    fig = px.scatter(nutrients, y='Iodide (I) (µg)', x='Starch (g)', color='Category')
+    fig.show()
+
+    fig = px.scatter(nutrients, y='Iodide (I) (µg)', x='Dietary fibres (g)', color='Category')
+    fig.show()
+
+    fig = px.scatter(nutrients, y='Iodide (I) (µg)', x='Fat, total (g)', color='Category')
+    fig.show()
+
+    # Salt
+    fig = px.scatter(nutrients, y='Salt (NaCl) (g)', x='Protein (g)', color='Category')
+    fig.show()
+
+    fig = px.scatter(nutrients, y='Salt (NaCl) (g)', x='Starch (g)', color='Category')
+    fig.show()
+
+    fig = px.scatter(nutrients, y='Salt (NaCl) (g)', x='Dietary fibres (g)', color='Category')
+    fig.show()
+
+    fig = px.scatter(nutrients, y='Salt (NaCl) (g)', x='Fat, total (g)', color='Category')
+    fig.show()
+    """
+
+
+
+
+
